bcf359f006881973b8d2cc8587c33951a96073c2
Deraj <75500118+derajoxford@users.noreply.github.com>
2025-09-03 18:29:22 -0500

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,6 @@
 import { PrismaClient } from '@prisma/client';
 
 declare global {
-  // for dev/hot-reload safety – ignored in prod
   // eslint-disable-next-line no-var
   var __prisma__: PrismaClient | undefined;
 }
