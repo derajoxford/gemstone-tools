d6b4938eff66daa5ec74b110843babc1a83ac052
Deraj <75500118+derajoxford@users.noreply.github.com>
2025-09-03 19:39:19 -0500

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,19 +1,3 @@
 // src/db.ts
 import { PrismaClient } from '@prisma/client';
-
-declare global {
-  // eslint-disable-next-line no-var
-  var __prisma__: PrismaClient | undefined;
-}
-
-export const prisma: PrismaClient =
-  globalThis.__prisma__ ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
-
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.__prisma__ = prisma;
-}
-
-export default prisma;
+export const prisma = new PrismaClient();
