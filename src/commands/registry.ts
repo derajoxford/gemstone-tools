6124cded049153faa7d2350561587077f9b94dc3
Deraj <75500118+derajoxford@users.noreply.github.com>
2025-09-23 14:38:19 -0500

    Update registry.ts

--- src/commands/registry.ts
+++ src/commands/registry.ts
@@ -1,11 +1,31 @@
 // src/commands/registry.ts
-import * as treasury from './treasury';
-import * as treasury_add from './treasury_add';
-import * as treasury_bulk from './treasury_bulk';
-import * as pnw_set from './pnw_set';
-import * as pnw_bankpeek from './pnw_bankpeek'; 
+import type { RESTPostAPIChatInputApplicationCommandsJSONBody } from "discord.js";
+
+// Treasury + banking
+import * as treasury from "./treasury";
+import * as treasury_add from "./treasury_add";
+import * as treasury_bulk from "./treasury_bulk";
+
+// Safekeeping + withdrawals
+import * as balance from "./balance";
+import * as link_nation from "./link_nation";
+import * as safekeeping_edit from "./safekeeping_edit";
+import * as setup_alliance from "./setup_alliance";
+import * as set_review_channel from "./set_review_channel";
+import * as withdraw from "./withdraw";
+import * as withdraw_json from "./withdraw_json";
+import * as withdraw_list from "./withdraw_list";
+import * as withdraw_set from "./withdraw_set";
+
+// PnW integration
+import * as pnw_bankpeek from "./pnw_bankpeek";
 import * as pnw_tax_apply from "./pnw_tax_apply";
-import * as who from './who';
+import * as pnw_set from "./pnw_set";
+
+// (parked ones: pnw_preview, pnw_apply, pnw_cursor, etc. are disabled)
+
+// NEW: /who
+import * as who from "./who";
 
 export type CommandModule = {
   data?: { name?: string; toJSON?: () => any };
@@ -13,19 +33,39 @@ export type CommandModule = {
 };
 
 export const commandModules: CommandModule[] = [
+  // treasury
   treasury,
   treasury_add,
   treasury_bulk,
-  pnw_set,
+
+  // safekeeping + withdrawals
+  balance,
+  link_nation,
+  safekeeping_edit,
+  setup_alliance,
+  set_review_channel,
+  withdraw,
+  withdraw_json,
+  withdraw_list,
+  withdraw_set,
+
+  // pnw
   pnw_bankpeek,
   pnw_tax_apply,
+  pnw_set,
+
+  // misc
   who,
 ].filter((m) => m?.data && m?.execute);
 
-export const extraCommandsJSON = commandModules
-  .map((m) => m.data?.toJSON?.())
-  .filter(Boolean) as any[];
+export const commands: RESTPostAPIChatInputApplicationCommandsJSONBody[] =
+  commandModules.map((m) => m.data!.toJSON());
 
+export const executeMap = new Map<string, any>(
+  commandModules.map((m) => [m.data!.name!, m])
+);
+
+export const extraCommandsJSON: any[] = [];
 export function findCommandByName(name: string) {
   return commandModules.find((m) => m.data?.name === name);
 }
