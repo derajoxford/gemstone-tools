24453216a52c5259cbe90d82dff789544bd9448b
Deraj <75500118+derajoxford@users.noreply.github.com>
2025-09-26 15:41:30 -0500

    Create manual_adjust_log_channel.ts

---
+++ src/commands/manual_adjust_log_channel.ts
@@ -0,0 +1,26 @@
+// src/commands/manual_adjust_log_channel.ts
+import { SlashCommandBuilder, ChatInputCommandInteraction, PermissionFlagsBits, ChannelType } from "discord.js";
+import { setGuildSetting } from "../utils/settings.js";
+
+export const data = new SlashCommandBuilder()
+  .setName("manual_adjust_log_channel")
+  .setDescription("Set the channel where manual safekeeping adjustments are logged.")
+  .addChannelOption(o =>
+    o.setName("channel")
+     .setDescription("Pick a text channel for manual adjustment logs")
+     .addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
+     .setRequired(true)
+  )
+  .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+  .setDMPermission(false);
+
+export async function execute(i: ChatInputCommandInteraction) {
+  await i.deferReply({ ephemeral: true });
+  if (!i.guild) return i.editReply("Run this in a server.");
+
+  const ch = i.options.getChannel("channel", true);
+  await setGuildSetting(i.guild.id, "manual_adjust_log_channel_id", ch.id);
+  return i.editReply(`Manual adjustments will now be logged in <#${ch.id}>.`);
+}
+
+export default { data, execute };
